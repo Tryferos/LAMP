7766490eacc52c8384623718fe51382b8c569945
Tryferos <truf022@gmail.com>
2023-12-31 18:07:54 +0200

    test commit

---
+++ src/frontend/index.php
@@ -0,0 +1,13 @@
+
+<?php
+// This is a basic PHP file
+
+            // Define a function
+            function sayHello($name) {
+                return "Hello, " . $name;
+            }
+
+            // Use the function
+            echo sayHello("World");
+?>
+
